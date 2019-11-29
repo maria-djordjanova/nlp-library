dd6f3a22fc59f31ed48c3b76b0a6b901e7623ed4
maria <mariadjordjanova@marias-mbp.home>
2019-11-29 21:30:50 +0100

    add unit tests

--- nlplib/tests/utils/parser.py
+++ nlplib/tests/utils/parser.py
@@ -1,19 +1,39 @@
+import unittest
+
 from nlplib.utils.logger import Logger
 from nlplib.utils.parser import Parser
 
 
-class TestParser:
+class TestParser(unittest.TestCase):
 
     def __init__(self, name=None):
+        super(TestParser, self).__init__()
         self.logger = Logger(name)
 
-    def test_parse_file(self, filename):
-        self.logger.log.debug('testing parse file')
+    def parse_file(self, filename, author, title, pubDate):
+        self.logger.log.debug('TEST: parse file: ' + filename)
 
         document = Parser().parse_file(filename)
-        print(document.author)
+
+        self.assertEqual(document.author, author)
+        self.assertEqual(document.title, title)
+        self.assertEqual(document.pubDate, pubDate)
 
 
 if __name__ == "__main__":
-    parser = Parser()
-    parser.parse_file('../assets/data/sample1.txt')
+    parser = TestParser()
+
+    parser.parse_file('../assets/data/sample1.txt',
+                      'John Ronald Reuel Tolkien',
+                      'The Lord Of The Rings: The Fellowship of the Ring',
+                      '1954')
+
+    parser.parse_file('../assets/data/sample2.txt',
+                      'John Ronald Reuel Tolkien',
+                      'The Hobbit: Or There and Back Again',
+                      '1937')
+
+    parser.parse_file('../assets/data/sample3.txt',
+                      'Franz Kafka',
+                      'Metamorphosis',
+                      '1915')
